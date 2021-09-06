72e871d8283595f644daca2aef435b9ee3d2f40f
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-09-06 22:39:03 +0100

    chore: add user-defined-types file

---
+++ user-defined-types.sql
@@ -0,0 +1,13 @@
+CREATE DOMAIN COUNTRY_CODE AS 
+   VARCHAR NOT NULL UNIQUE CHECK (value !~ '\s');
+
+CREATE TABLE countries (
+    id serial primary key,
+    code COUNTRY_CODE,
+    name VARCHAR
+);
+
+INSERT INTO TABLE countries (code, name) 
+    VALUES('USA', 'United States of America'),
+          ('NZ', 'New Zealand'),
+          ('SA', 'South Africa');
